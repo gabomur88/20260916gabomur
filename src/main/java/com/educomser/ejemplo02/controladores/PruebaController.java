f129e15ab7f50852d1fa64996036f4c0fb569c59
gabomur88 <gmurillo.rivero@gmail.com>
2026-09-23 01:52:35 -0400

    Agregando variables de entorno al pipeline

--- src/main/java/com/educomser/ejemplo02/controladores/PruebaController.java
+++ src/main/java/com/educomser/ejemplo02/controladores/PruebaController.java
@@ -1,5 +1,6 @@
 package com.educomser.ejemplo02.controladores;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +8,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1")
 public class PruebaController {
+    @Value("${NOMBRE_USR}")
+    private String nombre;
+
+
     @GetMapping("/saludo")
     public String unEndpoint(){
-        return "Hola a todos";
+        return "Hola a todos y en especial a " + nombre;
     }
     @GetMapping("/despedida")
     public String otroEndpoint(){
